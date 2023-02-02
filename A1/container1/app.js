3cacaf75157d7d3cebb707242f0ab2225740d923
Arpit Ribadiya <ar304626@dal.ca>
2023-02-02 18:18:31 -0400

    Added Docker Assginment 1

---
+++ A1/container1/app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+var bodyParse = require('body-parser');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const fetch = require("node-fetch");
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/checksum", function(req,res,next){
+  var val = req.body.file;
+  if(val===null || val==="")
+  {
+    res.json(
+      {
+        file: null,
+        error: 'Invalid JSON input.'
+      })
+  }
+  else{
+    const path = 'files/' + val;
+    if (fs.existsSync(path)) {
+      const response = fetch("http://container2:4000/getFile", {
+          method: "post",
+          body: JSON.stringify({ filePath: val }),
+          headers: { "Content-Type": "application/json" },
+        })
+          .then(result => {
+            return result.json();
+          })
+          .then(rr => {
+            res.json(rr);
+          })
+          .catch(err => {
+            res.send(err);
+          });
+    } else {
+      res.json(
+        {
+          file: req.body.file,
+          error: 'File not found.'
+        })
+    }
+  }
+});
+
+app.listen(5000);
